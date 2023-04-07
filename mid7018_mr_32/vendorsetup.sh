dc74bea960166a4512c7205365eaa7bed78004ec
root <root@dev.myguest.virtualbox.org>
2023-04-07 08:42:11 -0400

    second commit

--- mid7018_mr_32/vendorsetup.sh
+++ mid7018_mr_32/vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_mid7018_mr_32-user
-add_lunch_combo omni_mid7018_mr_32-userdebug
+#add_lunch_combo omni_mid7018_mr_32-user
+#add_lunch_combo omni_mid7018_mr_32-userdebug
 add_lunch_combo omni_mid7018_mr_32-eng
