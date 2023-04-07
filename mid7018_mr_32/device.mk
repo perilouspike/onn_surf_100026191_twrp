40dd3403395908d2f715356d331a3369ed38459f
root <root@dev.myguest.virtualbox.org>
2023-04-07 07:24:05 -0400

    first commit

---
+++ mid7018_mr_32/device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/onn/mid7018_mr_32
+TARGET_SCREEN_WIDTH := 1024
+TARGET_SCREEN_HEIGHT := 600
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3G
