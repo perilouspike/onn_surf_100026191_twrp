caf380a5c9967797b9998a24162e3aa05dc3e8c4
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-04-09 18:52:04 +0530

    Trying to fix the build that not booting.

--- omni_mid7018.mk
+++ omni_mid7018.mk
@@ -8,8 +8,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 #$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 #$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from TWRP-common stuffs, if building TWRP.
